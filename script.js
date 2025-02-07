54b93197f43815405f9abc908059229f38492179
nishikanta7819 <nishikantabehera7819@gmail.com>
2025-02-07 13:48:17 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -2,21 +2,28 @@
 const toggleButton = document.getElementById("mode-toggle");
 const bodyElement = document.body;
 
+// Get the stored theme from localStorage
 const currentMode = localStorage.getItem("theme");
 
+// Apply the stored theme
 if (currentMode) {
   bodyElement.classList.add(currentMode);
+  toggleButton.innerHTML =
+    currentMode === "dark-mode" ? "Light Mode" : "Dark Mode";
+} else {
+  localStorage.setItem("theme", "light-mode"); // Default to light mode
 }
 
+// Toggle between Light and Dark mode
 toggleButton.addEventListener("click", () => {
   bodyElement.classList.toggle("dark-mode");
 
   if (bodyElement.classList.contains("dark-mode")) {
-    document.getElementById("mode-toggle").innerHTML = "Light Mode";
+    toggleButton.innerHTML = "Light Mode";
     localStorage.setItem("theme", "dark-mode");
   } else {
+    toggleButton.innerHTML = "Dark Mode";
     localStorage.setItem("theme", "light-mode");
-    document.getElementById("mode-toggle").innerHTML = "Dark Mode";
   }
 });
 
@@ -38,4 +45,16 @@ scrollButtons.forEach((button) => {
   });
 });
 
+
+// copyright year
 document.getElementById("year").textContent = new Date().getFullYear();
+
+// Custom Cursor------------
+// Select the custom cursor element
+const cursor = document.querySelector(".custom-cursor");
+
+// Smoothly update cursor position
+document.addEventListener("mousemove", (e) => {
+  cursor.style.left = `${e.clientX}px`;
+  cursor.style.top = `${e.clientY}px`;
+});
